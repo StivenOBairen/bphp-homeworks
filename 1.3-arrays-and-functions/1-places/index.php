d2c73174b90933656fd1453ccc22540f0aefb39c
StivenOBairen <ivigraven@gmail.com>
2019-06-15 00:19:43 +0500

    update index.php

--- 1.3-arrays-and-functions/1-places/index.php
+++ 1.3-arrays-and-functions/1-places/index.php
@@ -1,21 +1,21 @@
 <?php
 function generate($rows, $placesPerRow, $avaliableCount) {
     $map = [];
+    if (($rows * $placesPerRow) > $avaliableCount) {
+        echo 'Ошибка построения карты зала! Проверьте количество доступных мест.<br>';
+        return false;
+    }
     for ($i = 0; $i < $rows; $i++) {
         for ($j = 0; $j < $placesPerRow; $j++) {
             $map[$i][$j] = false;
         }
     }
-    if ($rows * $placesPerRow > $avaliableCount) {
-      return false;
-    } else {
-      return $map;
-    }
+    return $map;
 }
 
 function reserve($map, $row, $place) {
-    if ($map[$row + 1][$place + 1] === false) {         //usually humans start counting from '1', lets add +1 to the array indexes
-        $map[$row + 1][$place + 1] = true;
+  if ($map[$row - 1][$place - 1] === false) {         //usually humans start counting from '1', lets add -1 to the array indexes
+        $map[$row - 1][$place - 1] = true;
         return true;
     } else {
         return false;
@@ -24,12 +24,12 @@ function reserve($map, $row, $place) {
 
 $chairs = 50;
 $map = generate(5, 8, $chairs);
-$requireRow = 3;
-$requirePlace = 5;
 
-$reverve = reserve($map, $requireRow, $requirePlace);
+$requireRow = 5;
+$requirePlace = 8;
 
-logReserve($requireRow, $requirePlace, $reverve);
+$reserve = reserve($map, $requireRow, $requirePlace);
+logReserve($requireRow, $requirePlace, $reserve);
 
 function logReserve($row, $place, $result){
     if ($result) {
--- 1.3-arrays-and-functions/2-search/index.php
+++ 1.3-arrays-and-functions/2-search/index.php
@@ -1,21 +1,21 @@
 <?php
 function generate($rows, $placesPerRow, $avaliableCount) {
     $map = [];
+    if (($rows * $placesPerRow) > $avaliableCount) {
+        echo 'Ошибка построения карты зала! Проверьте количество доступных мест.<br>';
+        return false;
+    }
     for ($i = 0; $i < $rows; $i++) {
-        for($j = 0; $j < $placesPerRow; $j++) {
+        for ($j = 0; $j < $placesPerRow; $j++) {
             $map[$i][$j] = false;
         }
     }
-    if ($rows * $placesPerRow > $avaliableCount) {
-      return false;
-    } else {
-      return $map;
-    }
+    return $map;
 }
 
 function reserve($map, $row, $place) {
-    if ($map[$row + 1][$place + 1] === false) {
-        $map[$row + 1][$place + 1] = true;
+  if ($map[$row - 1][$place - 1] === false) {         //usually humans start counting from '1', lets add -1 to the array indexes
+        $map[$row - 1][$place - 1] = true;
         return true;
     } else {
         return false;
@@ -24,21 +24,21 @@ function reserve($map, $row, $place) {
 
 function findNear($nearPlaces, $map) {
     $rowCount = count($map);
-    $columnCount = count($map[0]);        //no matter what $map[] would we count, let it be $map[0]
+    $columnCount = count($map[0]);                  //no matter what $map[] would we count, let it be $map[0]
     for ($i = 0; $i < $rowCount; $i++) {
-        $place = 0;
+        $place = 0;                                 //$nearPlaces counter
         for ($j = 0; $j < $columnCount; $j++) {
             if ($j > $columnCount - $nearPlaces + 1) {
-                continue;                 //check first relevant indexes in a row and skip other ones
+                break;                              //stop checking relevant indexes in a row to skip other ones
             }
-            if ($map[$i][$j] === false) { //is targer place empty?
+            if ($map[$i][$j] === false) {           //is targer place empty?
                 $place++;
                 if ($place == $nearPlaces) {
                     echo 'Доступные места: ряд '. ($i+1) . ',' . ' места с ' . ($j+1 - $nearPlaces+1). ' по ' . ($j+1) . '<br></br>';
-                    continue;
+                    return;                         //need just first relevant result
                 }
             } else {
-                $place = 0;               //update counter for new iteration
+                $place = 0;                         //update counter for new iteration in a new row
             }
         }
     }
